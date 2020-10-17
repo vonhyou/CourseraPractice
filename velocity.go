c65a55d4b8c825a1f0ba0f98283ffec6f0c2b92b
vonhyou <vonhyou@yeah.net>
2020-10-17 23:05:00 +0800

    update

--- velocity.go
+++ velocity.go
@@ -9,9 +9,26 @@ func genDisplaceFn(a, v0, s0 float64) func(float64) float64 {
 }
 
 func main() {
-  fn1 := genDisplaceFn(10, 2, 1)
-  fmt.Println(fn1(1))
+  /*
+  		fn1 := genDisplaceFn(10, 2, 1)
+  	  fmt.Println(fn1(1))
 
-  fn2 := genDisplaceFn(5, 5, 3)
-  fmt.Println(fn2(1))
+  	  fn2 := genDisplaceFn(5, 5, 3)
+  		fmt.Println(fn2(1))
+  */
+
+  for {
+    var a, v0, s0 float64
+    fmt.Println("Input a, v0, s0 or type 0 0 0 for exit:")
+    fmt.Scanln(&a, &v0, &s0)
+
+    if a == 0 && v0 == 0 && s0 == 0 {
+      break
+    }
+    var t float64
+    fmt.Println("Input t please:")
+    fmt.Scanln(&t)
+    fn := genDisplaceFn(a, v0, s0)
+    fmt.Println("Ans:", fn(t))
+  }
 }
